e8a1d6b7f5f8bb1ac3c3da8eafac9ca5a7eb0587
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-03-12 11:58:43 +0300

    last commit

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,18 @@ public class Main {
         GameProgress gameProgressMike = new GameProgress(55, 3, 23, 257.56);
         GameProgress gameProgressTony = new GameProgress(100, 9, 7, 97.23);
 
-
+        /**
+         * Сериализация
+         */
         System.out.println("Идёт сохранение...");
         GameProgress.saveGame("C://JAVA//Installation//Games//savegames//save1.dat", gameProgressJhon);
         GameProgress.saveGame("C://JAVA//Installation//Games//savegames//save2.dat", gameProgressMike);
         GameProgress.saveGame("C://JAVA//Installation//Games//savegames//save3.dat", gameProgressTony);
         System.out.println("Сохранение успешно");
 
-
+        /**
+         * Архивация файлов
+         */
         System.out.println("Идёт архивация сохраняемых файлов...");
         GameProgress.zipFiles("C://JAVA//Installation//Games//savegames//save1.zip",
                 "C://JAVA//Installation//Games//savegames//save1.dat");
@@ -29,6 +33,9 @@ public class Main {
         GameProgress.openZip("C://JAVA//Installation//Games//savegames//", "save2.zip");
         GameProgress.openZip("C://JAVA//Installation//Games//savegames//", "save3.zip");
 
+        /**
+         * Десериализация файлов
+         */
         GameProgress.openProgress("C://JAVA//Installation//Games//savegames//save1.dat");
         GameProgress.openProgress("C://JAVA//Installation//Games//savegames//save2.dat");
         GameProgress.openProgress("C://JAVA//Installation//Games//savegames//save3.dat");
